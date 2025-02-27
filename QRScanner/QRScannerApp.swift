959240b136b286ea50aff96d778f08600042e70f
Arnav Podichetty <arnav.p2024@gmail.com>
2025-02-27 15:00:12 -0800

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Arnav Podichetty on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
